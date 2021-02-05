6a522b38936217b64e2b008658fce45cf2621989
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-02-05 17:11:21 +0100

    wqeeqwed

---
+++ cmake-build-debug/CMakeFiles/Projects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projects.dir/main.cpp.obj"
+  "Projects.exe"
+  "Projects.exe.manifest"
+  "Projects.pdb"
+  "libProjects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hello, World!\n";
+    cout << "C++ beginner stuff\n\n";
+
+    //string er ikke med stort s som i java
+    //int, double, char er som i java
+    //boolean hedder bool i c++
+
+
+    //println hedder cout i c++
+
+
+    //alle operators (+-*/%) fungerer som i java\n"
+
+    //final keyword i java hedder const i c++
+    const int constant1 = 5;
+
+    //cout << er output, cin >> er input, det svarer til system.in i java
+
+    //Lommeregner
+    int x,y, sum;
+    cout << "indtast tal";
+    cin >> x;
+    cout << "indtast et til tal";
+    cin >> y;
+    sum = x+y;
+    cout << sum;
+    return 0;
+}
+
